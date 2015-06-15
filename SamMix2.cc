effac998716e31896ce53f8d189972c7668bd15a
SamAbernethy <12sa92@queensu.ca>
2015-06-15 12:38:23 +0200

    SamMix2

---
+++ SamMix2.cc
@@ -0,0 +1,269 @@
+#include "/home/sam/work/SamMix2.h"
+#include <iostream>
+#include <string>
+#include "TH1D.h"
+#include "TFile.h"
+#include "TCanvas.h"
+
+/* CURRENT ISSUES TO SOLVE:
+ * Pi0/acqu data for carbon/butanol in correct locations (~/work/acqu_data or ~/work/Pi0_data)
+ *      Labels on all data (Acqu_CBTaggTAPS_ or Pi0_CBTaggTAPS_)
+ * InitialCarbon first carbon file
+ *      TH1s C3500_0, C3500_1,   C_MissMass_0,   C_MissMass_1; with:
+ *           Theta_0, Theta_1, MM_pi0_n_2g_h0, MM_pi0_n_2g_h1
+ * CarbonLoop all carbon files
+ *      TH1s  Carb_0,  Carb_1; with:
+ *           Theta_0, Theta_1
+ *      TTree      AcqTree; with:
+ *            treeRawEvent
+ * Asymmetry all butanol files
+ *      TH1s BThet_0, BThet_1,   B_MissMass_0,   B_MissMass_1; with:
+ *           Theta_0, Theta_1, MM_pi0_n_2g_h0, MM_pi0_n_2g_h1
+ *      TTree     Acqu_but; with:
+ *            treeRawEvent
+ * */
+
+ofstream fout("data.txt"); // output for asymmetry data
+
+Int_t mix2()
+{
+    Int_t n_bin; // which theta bin
+    Int_t n_file; // number of files for Butanol
+    Int_t max_carb; // number of files for Carbon
+
+    ppi0 PolPar; // initialize PolPar, of class ppi0
+    std::cout << "Initializing Carbon data... " << endl;
+    std::cout << "Number of Files for Carbon data: ";
+    std::cin >> max_carb;
+    if ((!max_carb) || (max_carb == 0)) {
+        std::cout << "Number of files incorrectly specified. Try again." << endl;
+        return 1;
+    } else {
+        std::cout << "Number of files successfully specified." << endl;
+    }
+
+    // INITIALIZING CARBON
+    std::cout << "Carbon run number to start with: ";
+    std::cin >> carbonstart;
+    if (!carbonstart) {
+        std::cout << "Carbon run number not specified. Try again." << endl;
+        return 1;
+    } else {
+        std::cout << "Carbon run number successfully specified." << endl;
+        PolPar.InitialCarbon();
+    }
+
+    // CARBON LOOP
+    for (Int_t Carb_index = 0; Carb_index < max_carb; Carb_index++) {
+        PolPar.CarbonLoop(Carb_index);
+    }
+
+    std::cout << " " << endl;
+    std::cout << "Total Number of Carbon Background Entries Found: " << PolPar.GetCarbEntries() << endl;
+
+    // BUTANOL
+    std::cout << " " << endl;
+    std::cout << "Number of files for Butanol data: ";
+    std::cin >> n_file;
+    if ((!n_file) || (n_file == 0)) {
+        std::cout << "Number of files for Butanol data incorrectly specified. Try again." << endl;
+        return 1;
+    } else {
+        std::cout << "Number of files for Butanol data successfully specified." << endl;
+        std::cout << " " << endl;
+        std::cout << "Which theta bin to use: ";
+        std::cin >> n_bin;
+        if ((!n_bin) || (n_bin == 0) || (n_bin == 1)) {
+            std::cout << "Theta bin incorrectly specified. Try again." << endl;
+            return 1;
+        } else {
+            std::cout << "Theta bin successfully specified." << endl;
+            PolPar.Setn_bin(n_bin); // simply sets n_bin in PolPar equal to this specified n_bin
+            std::cout << "Butanol run number to start with: ";
+            std::cin >> butanolstart;
+            if (!butanolstart) {
+                std::cout << "Butanol run number not specified. Try again." << endl;
+                return 1;
+            } else {
+            std::cout << " " << endl;
+            std::cout << "Starting Butanol file input loop... " << endl;
+            for (Int_t i = 1; i <= n_file; i++) {
+                PolPar.Asymmetry(i);
+            }
+            fout.close();
+            PolPar.Graph();
+            std::cout << "Success!" << endl;
+            }
+        }
+    }
+}
+
+ppi0 :: ppi0() {} // what does this really do?
+ppi0 :: ~ppi0() {}
+
+void ppi0 :: InitialCarbon()
+{
+    TString carbnumber = Form("%d", carbonstart);
+    FirstCarbonLocation = "/home/sam/work/Pi0_data/Carbon/" + "Pi0_CBTaggTAPS_" + carbnumber + ".root";
+    TFile FirstFile (FirstCarbonLocation); // call on the first root file as Car_3500
+
+// TH1s C3500_0, C3500_1 need to exist in Car_3500
+// Theta_1 and Theta_0 need to exist in C3500_1, C3500_0
+    FirstFile.GetObject("Theta_1", C3500_1); // get Theta_1 from TH1 C3500_1
+    FirstFile.GetObject("Theta_0", C3500_0); // get Theta_0 from TH1 C3500_0
+    C3500_1->SetDirectory(0); // detaches this so that you can open and close root files without destroying it
+    C3500_0->SetDirectory(0);
+
+// TH1s C_MissMass_1, C_MissMass_0 NEED TO EXIST IN Car_3500
+// MM_pi0_n_2g_h1 and MM_pi0_n_2g_h0 need to exist in C_MissMass_1, C_MissMass_0
+    FirstFile.GetObject("MM_pi0_n_2g_h1", C_MissMass_1);
+    FirstFile.GetObject("MM_pi0_n_2g_h0", C_MissMass_0);
+    C_MissMass_1->SetDirectory(0);
+    C_MissMass_0->SetDirectory(0);
+}
+
+// LOOPING CARBON
+void ppi0 :: CarbonLoop(Int_t j)
+{
+// Carbon data from acqu and Pi0 must exist in the following locations:
+    TString AcqCarb_source = "/home/sam/work/acqu_data/Carbon";
+    TString Pi0Carb_source = "/home/sam/work/Pi0_data/Carbon";
+
+    Int_t n_carb_run = carbonstart + j;
+    TString carb_ext = Form("%d", n_carb_run);
+
+// acqu data must be called "Acqu_CBTaggTAPS_", while Pi0 data must be "Pi0_CBTaggTAPS_"
+    acqu_Carbon = AcqCarb_source + "Acqu_CBTaggTAPS_" + carb_ext + ".root";
+    Pi0_Carbon = Pi0Carb_source + "Pi0_CBTaggTAPS_" + carb_ext + ".root";
+
+    std::cout << "Checking for Carbon files... " << endl;
+    ifstream Afile(acqu_Carbon);
+    if (!Afile) {
+        std::cout << "Acqu Carbon files not found for file " << acqu_Carbon << endl;
+        return;
+    }
+    ifstream Cfile(Pi0_Carbon);
+    if (!Cfile) {
+        std::cout << "Pi0 Carbon files not found for file " << Pi0_Carbon << endl;
+        return;
+    }
+    std::cout << "Found!" << endl;
+
+    TFile Pi0Carb (Pi0_Carbon);
+    TFile AcqCarb (acqu_Carbon);
+
+// TH1s Carb_1 and Carb_0 must exist in Pi0Carb
+// Theta_1 and Theta_0 must exist in Carb_1, Carb_0
+    Pi0Carb.GetObject("Theta_1", Carb_1); // get Theta_1 from TH1 Carb_1
+    Pi0Carb.GetObject("Theta_0", Carb_0);
+    Carb_1->SetDirectory(0); // detach histogram
+    Carb_0->SetDirectory(0);
+
+    C3500_1->Add(Carb_1,1); // add Carb_1 to the Carbon stack
+    C3500_0->Add(Carb_0,1);
+
+// AcqTree must exist within AcqCarb, with treeRawEvent within it
+    AcqCarb.GetObject("treeRawEvent", AcqTree);
+    std::cout << "For the run from " << acqu_Carbon << ", the number of Acqu Entries is: " << AcqTree -> GetEntries() << endl;
+    std::cout << "Carbon bin content: " << C3500_1 -> GetBinContent(n_bin) << endl;
+    CarbEvnt += AcqTree -> GetEntries();
+}
+
+// ASYMMETRY WITH BUTANOL DATA
+void ppi0 :: Asymmetry(Int_t index)
+{
+    Int_t n_but_run = butanolstart + index;
+    TString but_ext = Form("%d", n_but_run);
+
+// Butanol data must exist for acqu and Pi0 as specified:
+    TString AcqBut_source = "/home/sam/work/acqu_data/Butanol/";
+    TString Pi0But_source = "/home/sam/work/Pi0_data/Butanol/";
+    TString histogram_source = "/home/sam/work/histograms/";
+
+// Pi0 data must have "Pi0_CBTaggTAPS", while acqu data must have "Acqu_CBTaggTAPS_"
+    Pi0_Butanol = Pi0But_source + "Pi0_CBTaggTAPS_" + but_ext + ".root";
+    acqu_Butanol = AcqBut_source + "Acqu_CBTaggTAPS_" + but_ext + ".root";
+
+    std::cout << "Checking for Butanol files... " << endl;
+    ifstream Bfile(Pi0_Butanol);
+    if (!Bfile) {
+        std::cout << "Pi0 Butanol files not found for file " << Pi0_Butanol << endl;
+        return;
+    }
+    ifstream Dfile(acqu_Butanol);
+    if (!Dfile) {
+        std::cout << "Acqu Butanol files not found for file " << acqu_Butanol << endl;
+        return;
+    }
+    std::cout << "Found!" << endl;
+
+    TFile Pi0But(Pi0_Butanol);
+    TFile AcqBut(acqu_Butanol);
+
+// TTree Acqu_but must exist in AcqBut, with treeRawEvent within it
+    AcqBut.GetObject("treeRawEvent", Acqu_but);
+    ButaEvnt = Acqu_but->GetEntries();
+
+    if(ButaEvnt < 4.0e+6) {
+        std::cout << "Butanol event count is too low. Check file " << acqu_Butanol << endl;
+        return; // this number was chosen by Dylan
+    }
+
+// TH1s BThet_1, BThet_0, B_MissMass_1, B_MissMass_0 must exist within Pi0But
+// Theta_1, Theta_0, MM_pi0_n_2g_h1, MM_pi0_n_2g_h0 must exist as specified
+    Pi0But.GetObject("Theta_1", BThet_1);
+    Pi0But.GetObject("Theta_0", BThet_0);
+    Pi0But.GetObject("MM_pi0_n_2g_h1", B_MissMass_1);
+    Pi0But.GetObject("MM_pi0_n_2g_h0", B_MissMass_0);
+
+    TFile hist(histogram_source + "histo" + but_ext + ".root", "RECREATE");
+    B_MissMass_1->Add(C_MissMass_1, (-1)*Scale());
+    B_MissMass_0->Add(C_MissMass_0, (-1)*Scale());
+    BThet_1->Add(C3500_1, (-1)*Scale());
+    BThet_0->Add(C3500_0, (-1)*Scale());
+    B_MissMass_1->Write();
+    B_MissMass_0->Write();
+    BThet_1->Write();
+    BThet_0->Write();
+
+    yield_0 = BThet_0 -> GetBinContent(n_bin);
+    yield_1 = BThet_1 -> GetBinContent(n_bin);
+    if (yield_0 < 0) {
+        std::cout << "Yield for helicity 0 was negative for file " << Pi0_Butanol << endl;
+        yield_0 = yield_0*(-1);
+    }
+    if (yield_1 < 0) {
+        std::cout << "Yield for helicity 1 was negative for file " << Pi0_Butanol << endl;
+        yield_1 = yield_1*(-1);
+    }
+    yield_0_e = BThet_0 -> GetBinError(n_bin);
+    yield_1_e = BThet_1 -> GetBinError(n_bin);
+
+    asym = (yield_0 - yield_1) / (yield_0 + yield_1);
+    err = (2./(pow(yield_0 + yield_1, 2.)))*sqrt(pow(yield_0, 2.)*pow(yield_1_e, 2.) + pow(yield_1, 2.)*pow(yield_0_e, 2.));
+
+    std::cout << "The run was: " << Pi0_Butanol << endl;
+    std::cout << "The number of Carbon entries was: " << CarbEvnt << endl;
+    std::cout << "The number of Butanol entries was: " << ButaEvnt << endl;
+    std::cout << "Therefore, the scale used was: " << Scale() << endl;
+    std::cout << "The data written to data.txt is: " << n_but_run << " " << asym << " " << err << endl;
+    fout << n_but_run << " " << asym << " " << err << endl;
+    hist.Close();
+}
+
+// GRAPHING
+void ppi0 :: Graph()
+{
+    gROOT->SetStyle("Plain");
+    TCanvas *c1 = new TCanvas();
+    c1->SetGrid();
+    TGraphErrors data("data.txt", "%lg %lg %lg"); // graph the run number, asymmetry, and error
+    data.SetTitle("Frozen Spin Target Polarization/Asymmetry ; Run Number; #Sigma P_{#gamma}");
+    data.SetMarkerStyle(kCircle);
+    data.SetFillColor(0);
+    data.SetLineColor(56);
+    data.DrawClone();
+    c1->Print("MyGraph.png", "png");
+    TFile f("data.root", "recreate"); // f is currently unused
+    data.Write();
+}
---
+++ SamMix2.h
@@ -0,0 +1,58 @@
+#ifndef SAMMIX2_H
+#define SAMMIX2_H
+
+#include "TFile.h" // this will need to be run somewhere with TFile.h
+
+class ppi0
+{
+private:
+
+    TH1* C3500_0; // within Car_3500
+    TH1* C3500_1; // within Car_3500
+    TH1* Carb_0; // within Pi0Carb, which is found in Pi0_Carbon
+    TH1* Carb_1; // within Pi0Carb, which is found in Pi0_Carbon
+    TH1* BThet_0; // within Pi0But, which is found in Pi0_Butanol
+    TH1* BThet_1; // within Pi0But, which is found in Pi0_Butanol
+    TH1* C_MissMass_1; // within Car_3500, the first root file
+    TH1* C_MissMass_0; // within Car_3500, the first root file
+    TH1* B_MissMass_1; // within Pi0But, which is found in Pi0_Butanol
+    TH1* B_MissMass_0; // within Pi0But, which is found in Pi0_Butanol
+
+    TTree* AcqTree; // within acqu_Carbon, used to find CarbEvnt
+    TTree* Acqu_but; // within acqu_Butanol, used to find ButEvnt
+
+    Int_t n_bin; // theta bin chosen
+    Int_t n_file; // number of butanol files
+    Int_t max_carb; // number of carbon files
+    Int_t carbonstart; // run number to start at for carbon
+    Int_t butanolstart; // run number to start at for butanol
+
+    Double_t ButaEvnt; // number of butanol events
+    Double_t CarbEvnt; // number of carbon events
+
+    TString FirstCarbonLocation; // location of the first root file used for carbon
+    TString Pi0_Carbon; // formerly Pi0file, where post-GoAT carbon data is found
+    TString acqu_Carbon; // formerly acqu_run, where pre-GoAT carbon data is found
+    TString Pi0_Butanol; // formerly but_run, where post-GoAT butanol data is found
+    TString acqu_Butanol; // formerly acq_run, where pre-GoAT butanol data is found
+
+    Double_t yield_0; // yield within a given theta bin for helicity 0
+    Double_t yield_1; // yield within a given theta bin for helicity 1
+    Double_t yield_0_e; // error on yield_0
+    Double_t yield_1_e; // error on yield_1
+    Double_t asym; // asymmetry from yields
+    Double_t err; // error on asymmetry
+
+public:
+    ppi0(); // class constructor
+    ~ppi0(); // class destructor
+    void Setn_bin(Int_t i) { n_bin = i; } // simple void to assign n_bin to n_bin, archaic from Dylan
+    void CarbonLoop(Int_t j); // void to loop through carbon data
+    void Asymmetry(Int_t index); // void to calculate asymmetry from butanol data
+    void Graph(); // graph it and write into root file
+    void InitialCarbon(); // void to initialize one root file as stack base
+    Double_t GetCarbEntries() { return CarbEvnt; } // self-explanatory
+    Double_t Scale() { return ButaEvnt/CarbEvnt; } // Why this scaling from Dylan?
+};
+
+#endif // SAMMIX2_H
