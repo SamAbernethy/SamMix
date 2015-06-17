4790fac82f50e931445af8bd815f4e3f0b4302fc
SamAbernethy <12sa92@queensu.ca>
2015-06-17 18:01:39 +0200

    End of June 17th

--- SamMix2.cc
+++ SamMix2.cc
@@ -96,25 +96,22 @@ void ppi0 :: InitialCarbon()
     FirstCarbonLocation = FirstSource + "pi0-samApril_CBTaggTAPS_" + carbnumber + ".root";
     ifstream Afile(FirstCarbonLocation);
     if (!Afile) {
-        std::cout << "No file there" << endl;
+        std::cout << "Initial Carbon file not found." << endl;
         return;
     }
     TFile FirstFile (FirstCarbonLocation); // call on the first root file as FirstFile
     std::cout << "First file found!" << endl;
 
-// TH1s C3500_0, C3500_1 need to exist in FirstFile THIS IS AN ISSUE
-// Theta_1 and Theta_0 need to exist in C3500_1, C3500_0
     FirstFile.GetObject("Theta_1", C3500_1); // get Theta_1 from TH1 C3500_1
     FirstFile.GetObject("Theta_0", C3500_0); // get Theta_0 from TH1 C3500_0
     C3500_1->SetDirectory(0); // detaches this so that you can open and close root files without destroying it
     C3500_0->SetDirectory(0);
 
-// TH1s C_MissMass_1, C_MissMass_0 NEED TO EXIST IN Car_3500
-// MM_pi0_n_2g_h1 and MM_pi0_n_2g_h0 need to exist in C_MissMass_1, C_MissMass_0
-//    FirstFile.GetObject("MM_pi0_n_2g_h1", C_MissMass_1);
-//    FirstFile.GetObject("MM_pi0_n_2g_h0", C_MissMass_0);
-//    C_MissMass_1->SetDirectory(0);
-//    C_MissMass_0->SetDirectory(0);
+    // MM_pi0_n_2g_h1 and MM_pi0_n_2g_h0 need to exist in FirstFile
+/*  FirstFile.GetObject("MM_pi0_n_2g_h1", C_MissMass_1);
+    FirstFile.GetObject("MM_pi0_n_2g_h0", C_MissMass_0);
+    C_MissMass_1->SetDirectory(0);
+    C_MissMass_0->SetDirectory(0); */
 }
 
 // ******************************************************************************************************
@@ -123,14 +120,14 @@ void ppi0 :: InitialCarbon()
 // LOOPING CARBON
 void ppi0 :: CarbonLoop(Int_t j)
 {
-// Carbon data from acqu and Pi0 must exist in the following locations:
+    // Carbon data from acqu and Pi0 must exist in the following locations:
     TString AcqCarb_source = "/local/raid0/work/aberneth/a2GoAT/Apr2014/";
     TString Pi0Carb_source = "/local/raid0/work/aberneth/a2GoAT/postreconApril/";
 
     Int_t n_carb_run = carbonstart + j;
     TString carb_ext = Form("%d", n_carb_run);
 
-// acqu data must be called "Acqu_CBTaggTAPS_", while Pi0 data must be "pi0-samApril_CBTaggTAPS_"
+    // acqu data must be called "Acqu_CBTaggTAPS_", while Pi0 data must be "pi0-samApril_CBTaggTAPS_"
     acqu_Carbon = AcqCarb_source + "Acqu_CBTaggTAPS_" + carb_ext + ".root";
     Pi0_Carbon = Pi0Carb_source + "pi0-samApril_CBTaggTAPS_" + carb_ext + ".root";
 
@@ -150,9 +147,8 @@ void ppi0 :: CarbonLoop(Int_t j)
     TFile Pi0Carb (Pi0_Carbon);
     TFile AcqCarb (acqu_Carbon);
 
-// TH1s Carb_1 and Carb_0 must exist in Pi0Carb
-// Theta_1 and Theta_0 must exist in Carb_1, Carb_0
-    Pi0Carb.GetObject("Theta_1", Carb_1); // get Theta_1 from TH1 Carb_1
+    // Theta_1 and Theta_0 must exist in Pi0Carb
+    Pi0Carb.GetObject("Theta_1", Carb_1); // get Theta_1 from Pi0Carb
     Pi0Carb.GetObject("Theta_0", Carb_0);
     Carb_1->SetDirectory(0); // detach histogram
     Carb_0->SetDirectory(0);
@@ -160,8 +156,7 @@ void ppi0 :: CarbonLoop(Int_t j)
     C3500_1->Add(Carb_1,1); // add Carb_1 to the Carbon stack
     C3500_0->Add(Carb_0,1);
 
-// AcqTree must exist within AcqCarb, with treeRawEvent within it
-    AcqCarb.GetObject("trigger", AcqTree);
+    AcqCarb.GetObject("trigger", AcqTree); // trigger (OR A BETTER TREE) must be in AcqCarb
     std::cout << "For run number " << n_carb_run << ", the number of Acqu Entries is: " << AcqTree -> GetEntries() << endl;
     std::cout << "Carbon bin content for helicity 1: " << C3500_1 -> GetBinContent(theta_bin) << endl;
     std::cout << "Carbon bin content for helicity 0: " << C3500_0 -> GetBinContent(theta_bin) << endl;
@@ -177,12 +172,12 @@ void ppi0 :: Asymmetry(Int_t index)
     Int_t n_but_run = butanolstart + index;
     TString but_ext = Form("%d", n_but_run);
 
-// Butanol data must exist for acqu and Pi0 as specified:
+    // Butanol data must exist for acqu and Pi0 as specified:
     TString AcqBut_source = "/local/raid0/work/aberneth/a2GoAT/May2014/";
     TString Pi0But_source = "/local/raid0/work/aberneth/a2GoAT/postreconMay/";
     TString histogram_source = "/local/raid0/work/aberneth/a2GoAT/histograms/";
 
-// Pi0 data must have "Pi0_CBTaggTAPS", while acqu data must have "Acqu_CBTaggTAPS_"
+    // Pi0 data must have "pi0-samMay_CBTaggTAPS", while acqu data must have "Acqu_CBTaggTAPS_"
     Pi0_Butanol = Pi0But_source + "pi0-samMay_CBTaggTAPS_" + but_ext + ".root";
     acqu_Butanol = AcqBut_source + "Acqu_CBTaggTAPS_" + but_ext + ".root";
 
@@ -202,8 +197,7 @@ void ppi0 :: Asymmetry(Int_t index)
     TFile Pi0But(Pi0_Butanol);
     TFile AcqBut(acqu_Butanol);
 
-// TTree Acqu_but must exist in AcqBut, with treeRawEvent within it
-    AcqBut.GetObject("trigger", Acqu_but);
+    AcqBut.GetObject("trigger", Acqu_but); // trigger (OR A BETTER TREE) must be in AcqBut
     ButaEvnt = Acqu_but->GetEntries();
 
     if(ButaEvnt < 4.0e+6) {
@@ -211,22 +205,21 @@ void ppi0 :: Asymmetry(Int_t index)
         return;
     }
 
-// TH1s BThet_1, BThet_0, B_MissMass_1, B_MissMass_0 must exist within Pi0But
-// Theta_1, Theta_0, MM_pi0_n_2g_h1, MM_pi0_n_2g_h0 must exist as specified
-    Pi0But.GetObject("Theta_1", BThet_1);
+    Pi0But.GetObject("Theta_1", BThet_1); // get Theta_1 from Pi0But
     Pi0But.GetObject("Theta_0", BThet_0);
-//    Pi0But.GetObject("MM_pi0_n_2g_h1", B_MissMass_1);
-//    Pi0But.GetObject("MM_pi0_n_2g_h0", B_MissMass_0);
-
-    TFile hist(histogram_source + "histo" + but_ext + ".root", "RECREATE");
-//    B_MissMass_1->Add(C_MissMass_1, (-1)*Scale());
-//    B_MissMass_0->Add(C_MissMass_0, (-1)*Scale());
     BThet_1->Add(C3500_1, (-1)*Scale());
     BThet_0->Add(C3500_0, (-1)*Scale());
-//    B_MissMass_1->Write();
-//    B_MissMass_0->Write();
     BThet_1->Write();
     BThet_0->Write();
+    TFile hist(histogram_source + "histo" + but_ext + ".root", "RECREATE");
+
+    // MM_pi0_n_2g_h1 must exist in Pi0But
+/*  Pi0But.GetObject("MM_pi0_n_2g_h1", B_MissMass_1);
+    Pi0But.GetObject("MM_pi0_n_2g_h0", B_MissMass_0);
+    B_MissMass_1->Add(C_MissMass_1, (-1)*Scale());
+    B_MissMass_0->Add(C_MissMass_0, (-1)*Scale());
+    B_MissMass_1->Write();
+    B_MissMass_0->Write(); */
 
     yield_0 = BThet_0 -> GetBinContent(theta_bin);
     yield_1 = BThet_1 -> GetBinContent(theta_bin);
