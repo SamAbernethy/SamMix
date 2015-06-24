3389d8fac0df49b7a0a529192efa833a547b4f71
SamAbernethy <12sa92@queensu.ca>
2015-06-24 15:46:12 +0200

    tracks tree

--- SamMix2.cc
+++ SamMix2.cc
@@ -49,7 +49,7 @@ Int_t mix2()
         std::cout << "Carbon run number not specified. Try again." << endl;
         return 1;
     } else {
-        std::cout << "Carbon run number successfully specified, as " << carbonstart << endl;
+        std::cout << "Carbon run number successfully specified." << endl;
         PolPar.SetCarbonStart(carbonstart);
         PolPar.InitialCarbon();
     }
@@ -167,7 +167,7 @@ void ppi0 :: CarbonLoop(Int_t j)
     C3500_1->Add(Carb_1,1); // add Carb_1 to the Carbon stack
     C3500_0->Add(Carb_0,1);
 
-    AcqCarb.GetObject("tagger", AcqTree); // trigger (OR A BETTER TREE) must be in AcqCarb
+    AcqCarb.GetObject("tracks", AcqTree); // trigger (OR A BETTER TREE) must be in AcqCarb
     std::cout << "For run number " << n_carb_run << ", the number of Acqu Entries is: " << AcqTree -> GetEntries() << endl;
     std::cout << "Carbon bin content for helicity 1: " << C3500_1 -> GetBinContent(theta_bin) << endl;
     std::cout << "Carbon bin content for helicity 0: " << C3500_0 -> GetBinContent(theta_bin) << endl;
@@ -214,7 +214,7 @@ void ppi0 :: Asymmetry(Int_t index)
     TFile Pi0But(Pi0_Butanol);
     TFile AcqBut(acqu_Butanol);
 
-    AcqBut.GetObject("tagger", Acqu_but); // trigger (OR A BETTER TREE) must be in AcqBut
+    AcqBut.GetObject("tracks", Acqu_but); // trigger (OR A BETTER TREE) must be in AcqBut
     ButaEvnt = Acqu_but -> GetEntries();
 
     Pi0But.GetObject("Theta_1", BThet_1); // get Theta_1 from Pi0But
